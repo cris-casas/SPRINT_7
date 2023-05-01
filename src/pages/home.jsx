0f9cde7d7db3299ac1bfa696dd2b20f559a2c3a9
Cris-casas-react <cristina.casas2@gmail.com>
2023-05-01 19:13:56 +0200

    description-home-add_nivel-1_ok

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div>
             <h1>Bienvenido!</h1>
-            <p>bla bla bla</p>
+            <p>Calcula el teu pressupost a mida segons els nostres serveis:</p>
             <Link to="/page2">
                 <button>Calcula</button>
             </Link>
